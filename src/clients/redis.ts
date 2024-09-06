6a4ce1be2eb17dc6e5db02e5692499d0c36f67f7
david_tsai <108356037@nccu.edu.tw>
2024-09-06 13:32:02 +0800

    move redis to src/clients

--- src/clients/redis.ts
+++ src/clients/redis.ts
@@ -1,4 +1,4 @@
-import Redis from "ioredis";
+import { Redis } from "ioredis";
 
 const redisClient = new Redis({
   host: process.env.REDIS_HOST || "localhost",
@@ -9,4 +9,4 @@ if (!redisClient) {
   throw new Error("Redis client not found");
 }
 
-export default redisClient;
+export { redisClient };
